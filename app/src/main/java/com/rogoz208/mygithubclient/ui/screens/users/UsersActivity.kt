3ef4f45ddc9e2fe4600cb8ede162d1badc1c0487
rogoz208 <rogoz208@gmail.com>
2022-01-27 00:04:15 +0300

    lesson-1: filled UsersActivity.kt recycler view by test values

--- app/src/main/java/com/rogoz208/mygithubclient/ui/screens/users/UsersActivity.kt
+++ app/src/main/java/com/rogoz208/mygithubclient/ui/screens/users/UsersActivity.kt
@@ -2,12 +2,15 @@ package com.rogoz208.mygithubclient.ui.screens.users
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.rogoz208.mygithubclient.R
 import com.rogoz208.mygithubclient.databinding.*
 import com.rogoz208.mygithubclient.domain.entities.UserEntity
+import com.rogoz208.mygithubclient.ui.screens.users.recycler.OnItemClickListener
 import com.rogoz208.mygithubclient.ui.screens.users.recycler.UsersAdapter
 import com.rogoz208.mygithubclient.ui.screens.users.recycler.UsersDiffCallback
 
@@ -21,18 +24,40 @@ class UsersActivity : AppCompatActivity(R.layout.activity_users) {
         super.onCreate(savedInstanceState)
 
         initRecyclerView()
-
+        fillRecyclerViewByTestValues()
     }
 
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
+        val onItemClickListener = object : OnItemClickListener{
+            override fun onItemClick(item: UserEntity, position: Int) {
+                Toast.makeText(this@UsersActivity, "${item.userName} is clicked", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onItemLongClick(item: UserEntity, itemView: View, position: Int) {
+                Toast.makeText(this@UsersActivity, "${item.userName} is long pressed", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        adapter.setOnItemClickListener(onItemClickListener)
+
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
     }
 
-    private fun fillRecyclerView(users: List<UserEntity>){
+    private fun fillRecyclerView(users: List<UserEntity>) {
         val usersDiffCallback = UsersDiffCallback(oldList = adapter.data, newList = users)
         val result = DiffUtil.calculateDiff(usersDiffCallback)
         adapter.data = users
         result.dispatchUpdatesTo(adapter)
+    }
+
+    private fun fillRecyclerViewByTestValues() {
+        val users = mutableListOf<UserEntity>()
+        for (i in 1..10) {
+            users.add(UserEntity("", "User-$i", ""))
+        }
+        fillRecyclerView(users)
+
     }
 }
