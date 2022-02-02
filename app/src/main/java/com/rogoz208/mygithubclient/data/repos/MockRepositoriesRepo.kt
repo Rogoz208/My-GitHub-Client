1565ac7eb79aa6777043d7df2f0502786773aaa2
rogoz208 <rogoz208@gmail.com>
2022-02-02 23:59:30 +0300

    lesson-3: implement Rx in MockRepositoriesRepo.kt

--- app/src/main/java/com/rogoz208/mygithubclient/data/repos/MockRepositoriesRepo.kt
+++ app/src/main/java/com/rogoz208/mygithubclient/data/repos/MockRepositoriesRepo.kt
@@ -3,9 +3,12 @@ package com.rogoz208.mygithubclient.data.repos
 import com.rogoz208.mygithubclient.domain.entities.RepositoryEntity
 import com.rogoz208.mygithubclient.domain.repos.RepositoriesRepo
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 
 class MockRepositoriesRepo : RepositoriesRepo {
     private val cache: MutableList<RepositoryEntity> = mutableListOf()
+
+    private val cacheSubject = BehaviorSubject.createDefault<List<RepositoryEntity>>(cache)
 
     init {
         fillRepoByTestValues()
@@ -20,12 +23,13 @@ class MockRepositoriesRepo : RepositoriesRepo {
     }
 
     override fun getRepositoriesObservable(userName: String): Observable<List<RepositoryEntity>> {
-        return Observable.empty() // todo
+        return cacheSubject
     }
 
     private fun fillRepoByTestValues() {
         for (i in 1..10) {
             cache.add(RepositoryEntity(i.toLong(), "Repository-$i"))
         }
+        cacheSubject.onNext(cache)
     }
 }
